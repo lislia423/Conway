1970c87170aa634c1aa056789e1bf6f7d73115b3
Lisa Liao <LisaLiao423@hotmail.com>
2019-04-17 11:55:06 -0500

    updates

--- conways/src/gameboard.js
+++ conways/src/gameboard.js
@@ -1,208 +1,301 @@
-// import React, { Component } from 'react';
-// import './gameboard.css';
+import React, { Component } from 'react';
+import './gameboard.css';
 
-// //EXAMPLE FOR USING STATE AND SETSTATE
 
-// // class Cell extends React.Component {
-// //   // Constructs a cell with a status initiall equal to dead
-// //   constructor(props) {
-// //     super(props);
-    
-// //     this.changeColorOnClick.bind(this);
-// //     this.state = {
-// //       living: "dead",
-// //       bgColor: "#FFFFFF",
-// //       cell_name: "deadCell"
-// //     }
-// //   }
-// //   // method for changing cell color and status on click
-// //   changeColorOnClick = () =>{
-// //     if(this.state.living === "dead"){
-// //       this.setState({living: "alive", cell_name: "liveCell"});
-// //       console.log("living: "+this.state.living + " cell name: "+this.state.cell_name);
-  
-// //     }
-// //     else{
-// //       this.setState({living: "dead", cell_name: "deadCell"});
-// //       console.log("living: "+this.state.living + " cell name: "+this.state.cell_name);
+class Cell extends React.Component {
+    constructor(props) {
+        super(props);
+        props = {
+            coord: [],
+            status: "dead"
+        }
+    }
+    changeColorOnClick = () => {
+        return(
+            this.props.changeColor(this.props.coord)
+        )
+    }
+    render(){
+        return(
+            <td className={this.props.status} onClick={this.changeColorOnClick} value={this.props.coord}></td>
+        )
+    }
+}
+class Gameboard extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            width: 50,
+            height: 50,
+            interval: 100,
+            cellMap: new Map(),
+            generation: 0,
+            running: false
+        }
+ 
+    }
+    changeCellColor = (c) =>{
+        let newM = this.state.cellMap;
+        let cellClicked = newM[c];
+        if(cellClicked === "dead"){
+            newM[c] = "alive";
+        }
+        else{
+            newM[c] = "dead";
+        }
+        this.setState({cellMap: newM});
+    }
+    renderCell(c){
+        let s = "";
+        let random = Math.floor(Math.random()*Math.floor(4));
+        if(random > 2){
+            s = "alive";
+        }
+        else{
+            s = "dead";
+        }
 
-// //     }
-    
-// //   }
-// class Cell extends React.Component {
-//   // Constructs a cell with a status initiall equal to dead
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       living: this.props.status
-//     }
-//   }
-//   // renders the new cell as a button
-//   render() {
-//     return (
-//       <button className="cell" style={{backgroundColor: this.props.color}}>{this.props.value}</button>
-//     );
-//   }
-// }
+        return <Cell coord = {c} status = {s} changeColor={this.changeCellColor} />;
 
-// class Gameboard extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.board = this.createBoard();
-//   }
-//   // creates a new cell object with value of i
-//   renderCell(i) {
-//   let status = "dead";
-//   let num = Math.floor(Math.random() * Math.floor(4));
-//   let color = "#FF0000";
-//   if (num > 2) {
-//     status = "alive";
-//   }
-//   if (status === "dead") {
-//     color = "#FFFFFF";
-//   }
-   
-//     return <Cell key = {i} status = {status} color={color}/>
-//   }
-
-//   createBoard = () => {
-//     let width = 20;
-//     let height = 20;
-//     let gameboard_mf = []
-//     let cell_key = 0;
-//     let array_key = 0;
-//     // Outer loop to create parent
-//     for (let i = 0; i < width; i++) {
-//       let row_cells = []
-//       //Inner loop to create children
-//       for (let j = 0; j < height; j++) {
+    }
+    renderDeadCell(c){
+        return <Cell coord = {c} status = "dead" changeColor={this.changeCellColor} />;
+    }
+    createBoard = () =>{
+        let gameboard_emp = [];
         
-//         row_cells.push(this.renderCell(cell_key))
-//         cell_key += 1;
-//       }
-//       //Create the parent and add the children
-//       gameboard_mf.push(<div key = {array_key} className="board-row">{row_cells}</div>)
-//       array_key += 1;
-//     }
-//     return gameboard_mf
-//   }
+        for(let i = 0; i < this.state.height; ++i){
+            let rowCell = [];
+            for(let j = 0; j < this.state.width; ++j){
+                rowCell.push(<Cell coord = {[i,j]} status = {this.state.cellMap[[i,j]]} changeColor={this.changeCellColor} />);             
+            }
+            gameboard_emp.push(<tr className="board-row">{rowCell}</tr>);
+        }
+        
+        return gameboard_emp;
+      }
 
-//   checkNeighbors = () => {
-//     let width = 20;
-//     let height = 20;
-//     // loop through all the cells on the board
-//     for (let row = 0; row < height; ++row) {
-//       for (let col = 0; col < height; ++col) {
-//         let cell_status = this.board[row].props.children[col].props.status;
+    randomBoard = () =>{
+        let gameboard_mf = [];
+        let newMap = this.state.cellMap;
+        for(let i = 0; i < this.state.height; ++i){
+            let rowCells = [];
+            for(let j = 0; j < this.state.width; ++j){
+                let c = this.renderCell([i,j]);
+                newMap[[i,j]] = c.props.status;
+                rowCells.push(c);
+            }
+            gameboard_mf.push(<tr className="board-row">{rowCells}</tr>);
+        }
+        this.setState({cellMap: newMap, generation: 0});
+       
+    }
 
-//         let left_bound = col-1;
-//         if (col === 0) {
-//           left_bound = col;
-//         }
-//         else {
-//           left_bound = col-1;
-//         }
+    checkNeighbors = (c) =>{
+        
+        let tempMap = this.state.cellMap;
+        let row = c[0];
+        let col = c[1];
 
-//         let right_bound = col+1;
-//         if (col > width-2) {
-//           right_bound = col;
-//         }
-//         else {
-//           right_bound = col+1;
-//         }
+        let left_bound = col -1;
+        if (col === 0) {
+            left_bound = col;
+        }
 
-//         let upper_bound = row-1;
-//         if (row == 0) {
-//           upper_bound = row;
-//         }
-//         else {
-//           upper_bound = row-1;
-//         }
+        let right_bound = col+1;
+        if (col > this.state.width - 2) {
+            right_bound = col;
+        }
 
-//         let lower_bound = row+1;
-//         if (row > height-2) {
-//           lower_bound = row;
-//         }
-//         else {
-//           lower_bound = row+1;
-//         }
+        let upper_bound = row - 1;
+        if (row === 0) {
+            upper_bound = row;
+        }
 
-//         let num_neighbors = 0;
+        let lower_bound = row+1;
+        if (row > this.state.height - 2) {
+            lower_bound = row;
+        }
+        let num_neighbors = 0;
 
-//         console.log(row +","+col+": ub->" + upper_bound + " lb-> " + lower_bound + " rib-> " + right_bound + " leb-> " + left_bound);
+        for (let i = upper_bound; i <= lower_bound; ++i) {
+            for (let j = left_bound; j <= right_bound; ++j) {
+                if ((i !== row) || (j !== col)) {
+                    let arr = [i,j]
+                    let cell_status = tempMap[arr];
+                    if (cell_status === 'alive') {
+                        num_neighbors += 1;
+                    }
+                }
+            }
+        }
+        return num_neighbors;
 
+    }
 
-//         for (let row_2 = upper_bound; row_2 <= lower_bound; ++row_2) {
-//           for (let col_2 = left_bound; col_2 <= right_bound; ++col_2 ) {
-//             if((row_2 != row) || (col_2 != col)) {
-//               let cell_status_2 = this.board[row_2].props.children[col_2].props.status;
-//               console.log("*" + row_2 + "," + col_2 + ": " + cell_status_2);
-//               if (cell_status_2 === "alive") {
-//                 num_neighbors+=1;
-//               }
-//             }
-//           }
-//         }
+    gameLogic = () =>{
+        let temp = new Map();
+        for (let i = 0; i < this.state.height; ++i) {
+            for (let j = 0; j < this.state.width; ++j) {
+                temp[[i,j]] = this.state.cellMap[[i,j]];
+            }
+        }
 
-//         console.log("Num Neighbors: " + num_neighbors);
-
-//         console.log("before checking: " + this.board[row].props.children[col].props.status);
-
-//         // Case 1
-//         if (cell_status === "alive" && num_neighbors < 2) {
-//           this.board[row].props.children[col].props.status = "dead";
-          
-//         }
-//         else if (cell_status === "alive" && num_neighbors > 3) {
-//           this.board[row].props.children[col].props.status = "dead";
-
-//         }
-//         else if (cell_status === "alive" && num_neighbors <= 3 && num_neighbors >=2) {
-//           this.board[row].props.children[col].props.status = "alive";
-
-//         }
-//         else if (cell_status === "dead" && num_neighbors == 3) {
-//           this.board[row].props.children[col].props.status = "alive";
-
-//         }
-
-//         console.log(this.board[row]);
-//         //this.board[row].props.children[col].setState({living: "blue"});
-
-//         console.log("after checking: " + this.board[row].props.children[col].props.status);
-//         //console.log(this.board[row].props.children[col]);
-
-//       }
-//     }
+        
+        for(let i = 0; i < this.state.height; i++){
+            for(let j = 0; j < this.state.width; j++){
+                let numLiveNeighbors = this.checkNeighbors([i,j]);
+                let currentCell = this.state.cellMap[[i,j]];
+                if(currentCell === "alive" && numLiveNeighbors < 2){
+                    temp[[i,j]] = "dead";
+                }
+                else if(currentCell === "alive" && numLiveNeighbors > 3){
+                    temp[[i,j]] = "dead";
+                }
+                else if(currentCell === "alive" && numLiveNeighbors <= 3 && numLiveNeighbors >= 2){
+                    temp[[i,j]] = "alive";
+                }
+                else if(currentCell === "dead" && numLiveNeighbors === 3){
+                    temp[[i,j]] = "alive";
+                }
+            }
+        }
+        //update generations
+        let updateGeneration = this.state.generation;
+        updateGeneration+=1;
+        this.setState({cellMap: temp, generation: updateGeneration}); 
+    }
 
     
-//   }
 
-//   render() {
-//     return (
-//       <div>
-//         {this.board}
-//         {this.checkNeighbors()}
-//       </div>
-//     );
-//   }
-// }
+  //update board width
+  widthChange = (e) =>{  
+    if(this.state.running){
+        alert("Please stop the game before changing board size. (Max board size is 50x50)");
+    }
+    else{
+        // this.clear();
+        this.setState({width: e.target.value});
+        this.createBoard();
+    }
+  }
 
-// class Game extends React.Component {
-//   renderGameBoard = () => {
-//     return <Gameboard />
-//   }
+  //update board height
+  heightChange = (e) =>{
+    if(this.state.running){
+        alert("Please stop the game before changing board size. (Max board size is 50x50)");
+    }
+    else{
+        // this.clear();
+        this.setState({height: e.target.value});
+        this.createBoard();
+    }
+  }
 
-//   render() {
-//     return (
-//       <div className='game'>
-//         <div className = 'game-board'>
-//           {this.renderGameBoard()}
-//         </div>
-//       </div>
-//     )
-//   }
-// }
+  //update rendering intervals
+  intervalChange = (e) =>{
+    if(this.state.running){
+      alert("Please stop the game before changing intervals.");
+    }
+    else{
+      this.setState({interval: e.target.value});
+    }
+  }
 
+  //run game
+  run = () => {
+    if(!this.state.running){
+      let interv = setInterval(this.gameLogic, this.state.interval);
+      this.setState({running: true, interval: interv});
+    }
+  }
 
-// export default Game;
+  step = () =>{
+    if(this.state.running){
+      alert("Please stop the game before stepping.");
+    }
+    else{
+      this.gameLogic();
+    }
+  }
+  //stop game
+  stop = () => {
+    if(this.state.running){
+        
+      this.setState({running: false});
+      clearInterval(this.state.interval);
+      this.setState({interval: 100});
+    }
+  }
+  clear = () =>{
+    let newCellMap = this.state.cellMap;
+    let gameboard_m = []
+    for(let i = 0; i < this.state.height; ++i){
+        let rowCells = [];
+        for(let j = 0; j < this.state.width; ++j){
+            let c = this.renderDeadCell([i,j]);
+            let c_status = "dead";
+            rowCells.push(c);
+            newCellMap[[i,j]] = c_status;
+        }
+        gameboard_m.push(<tr className="board-row">{rowCells}</tr>)
+    }
+      this.setState({cellMap: newCellMap, generation: 0});
+  
+ 
+  }
+
+ 
+  //initial rendering data fetch
+  componentWillMount = () => {
+    this.randomBoard();
+    let interval = setInterval(this.gameLogic, this.state.interval);
+    this.setState({running: true, interval: interval});
+}
+
+    render() {
+        return(
+            <div>
+                <div className="title">Game of Life</div>
+                <div className="game">
+                <table>
+                    <tbody>
+                    {this.createBoard()}
+                    </tbody>
+                </table>
+                </div>
+                <br></br>
+                <br></br>
+                <div className="generations">
+                    <h3>Generation: {this.state.generation}</h3>
+                </div>
+                <br></br>
+                <br></br>
+                <div className="toolbar">
+                    <label> Board Width: 
+                        <input type="text" onChange={this.widthChange}></input>
+                    </label>
+                    <br></br>
+                    <br></br>
+                    <label> Board Height: 
+                        <input type="text" onChange={this.heightChange}></input>
+                    </label>
+                    <br></br>
+                    <br></br>
+                    <label> Intervals: 
+                        <input type="text" onChange={this.intervalChange}></input>
+                    </label>
+                    <br></br>
+                    <br></br>
+                    <button onClick={this.run}>Run</button>
+                    <button onClick={this.stop}>Stop</button>
+                    <button onClick={this.step}>step</button>
+                    <button onClick={this.randomBoard}>Randomize</button>
+                    <button onClick={this.clear}>Clear</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Gameboard;
